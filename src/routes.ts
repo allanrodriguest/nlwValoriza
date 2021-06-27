b3aa24f9ff54c50da6f20708fb2ff413f5b88dcb
Allan Rodrigues <allanrodriguest@hotmail.com>
2021-06-27 02:44:34 -0300

    jwt auth added

--- src/routes.ts
+++ src/routes.ts
@@ -2,13 +2,16 @@ import { Router } from "express";
 import { CreateUserController } from "./controllers/CreateUserController";
 import { CreateTagController } from "./controllers/CreateTagController";
 import { ensureAdmin } from "./middlewares/ensureAdmin";
+import { AuthenticateUserController } from "./controllers/AthenticateUserController";
 
 const router = Router();
 
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
+const authenticateUserController = new AuthenticateUserController();
 
 router.post("/users", ensureAdmin, createUserController.handle);
 router.post("/tags", createTagController.handle);
+router.post("/login", authenticateUserController.handle);
 
 export { router };
